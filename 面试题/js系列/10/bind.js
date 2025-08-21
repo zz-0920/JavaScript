51c959f84bf997f4ba3ce61ef3bea4825f1b3914
zz-0920 <zz19194997142@gmail.com>
2025-08-21 21:18:33 +0800

    js面试题

--- 面试题/js系列/10/bind.js
+++ 面试题/js系列/10/bind.js
@@ -1,8 +1,8 @@
 Function.prototype.myBind = function(context, ...args) {
   let _this = this
   return function F(...args2) {
-    if (this instanceof Function) { // 我是不是被 new 了
-      return _this
+    if (this instanceof F) { // 我是不是被 new 了
+      return new _this(...args, ...args2)
     } else {
       return _this.apply(context, [...args, ...args2])
     }
